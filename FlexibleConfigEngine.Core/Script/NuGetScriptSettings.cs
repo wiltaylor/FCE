43b8598531dc5ee8fe28aaed62710ea87a508993
Wil Taylor <wilfridtaylor@gmail.com>
2018-01-18 23:16:58 +1100

    Added default repository

--- FlexibleConfigEngine.Core/Script/NuGetScriptSettings.cs
+++ FlexibleConfigEngine.Core/Script/NuGetScriptSettings.cs
@@ -10,5 +10,7 @@ namespace FlexibleConfigEngine.Core.Script
         public IEnumerable<string> SupportedPlatforms => new[] { "netstandard2.0", "netstandard1.6", "netstandard1.5",
             "netstandard1.4", "netstandard1.3", "netstandard1.2", "netstandard1.1", "netstandard1.0", "netcoreapp1.0",
             "netcoreapp1.1", "netcoreapp2.0" };
+
+        public string DefaultRepository => "https://www.myget.org/F/fcepacks/api/v3/index.json";
     }
 }
